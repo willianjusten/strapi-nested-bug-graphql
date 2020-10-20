d106add898755d13db716598d760a3feb900fe6f
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2020-10-19 22:39:21 -0300

    Add code to reproduce nested bug
    If we use nested componentes with more than 3 levels, the GraphQL is returning `null`
    instead of the correct value.
    
    This issue was inserted on this PR:
    https://github.com/strapi/strapi/pull/7959/

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '09b94162fc9bf5dd3f220c45498f4098'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '092e21c2-1b97-4235-a742-d142831105cd'
+};
